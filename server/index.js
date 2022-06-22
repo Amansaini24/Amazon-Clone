a6cddaa8ed2f13cd26fc3b1b31e9d6ff141d8406
Aman <saini43432@gmail.com>
2022-06-22 15:07:21 +0530

    1st ommit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const authRouter = require('./routes/auth');
+
+const PORT = 3000;
+
+const app = express();
+const DB ="mongodb+srv://aman:aman123@cluster0.4lkpuqu.mongodb.net/?retryWrites=true&w=majority"
+
+app.use(express.json());
+app.use(authRouter);
+
+mongoose
+.connect(DB)
+.then(() => {
+    console.log("Connection Successful");
+})
+.catch((e) => {
+    console.log(e);
+});
+
+
+
+app.listen(PORT, "0.0.0.0",() => {
+    console.log(`connected at port ${PORT}`);
+});
